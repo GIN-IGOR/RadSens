1411f1bfab3e6417b3fffcf63ea0b23d3cb37603
Igor <56255453+GIN-IGOR@users.noreply.github.com>
2024-12-18 08:40:07 +0300

    Update main_ssd1306_radsens_start.py

--- main_ssd1306_radsens_start.py
+++ main_ssd1306_radsens_start.py
@@ -2,7 +2,7 @@
 import time
 # Библиотека для работы с пинами ввода-вывода
 from machine import Pin, I2C
-# Библиотека для работы со светодиодами WS2812
+# Библиотека для работы с модулем дисплея ssd1306
 from ssd1306_lib import SSD1306_I2C
 # Библиотека для работы с модулем RadSens
 from radsense_lib import CG_RadSens_I2C
@@ -13,41 +13,61 @@ scl_pin = 9
 sda_pin = 8
 
 # I2C
-# Указываем пин I2C
+# Объявление шины I2C
 i2c = I2C(0, scl=Pin(scl_pin), sda=Pin(sda_pin), freq=400000)
 
-# Указываем параметры OLED
+# Параметры модуля дисплея OLED
 oled_width = 128
 oled_height = 32
+# Объявление модуля дисплея OLED
 oled = SSD1306_I2C(oled_width, oled_height, i2c)
-# Указываем параметры radsense
+# Объявление модуля RadSens
 radsense = CG_RadSens_I2C(i2c)
 
+# Очистка содержимого на модуле дисплея
 oled.fill(0)
+# Вывод технической информации в порт
 print("ChipId: ", hex(radsense.getChipId()))
-oled.text("ChipId: " + str(hex(radsense.getChipId())), 0, 0, 1)
 print("Firmware: ", radsense.getFirmwareVersion())
+# Формирование информации для отображения на модуле дисплея
+oled.text("ChipId: " + str(hex(radsense.getChipId())), 0, 0, 1)
 oled.text("Firmware: " + str(radsense.getFirmwareVersion()), 0, 15, 1)
+# Показ содержимого на модуле дисплея
 oled.show()
+
+# Пауза 2 секунды
 time.sleep(2)
+
+# Очистка содержимого на модуле дисплея
 oled.fill(0)
+# Вывод технической информации в порт
 print("Sensitivity: ", radsense.getSensitivity())
-oled.text("Sens-ty: " + str(radsense.getSensitivity()), 0, 0, 1)
 print("LED state: ", radsense.getLedState())
+# Формирование информации для отображения на модуле дисплея
+oled.text("Sens-ty: " + str(radsense.getSensitivity()), 0, 0, 1)
 oled.text("LED state: " + str(radsense.getLedState()), 0, 15, 1)
+# Показ содержимого на модуле дисплея
 oled.show()
+
+# Пауза 2 секунды
 time.sleep(2)
+
+# Очистка содержимого на модуле дисплея
 oled.fill(0)
+# Показ содержимого на модуле дисплея
 oled.show()
 
 while True:
+    # Вывод информации в порт
     print("Dyn: ", radsense.getRadIntensyDynamic())
     print("Sta: ", radsense.getRadIntensyStatic())
     print("Pul: ", radsense.getNumberOfPulses())
+    # Очистка содержимого на модуле дисплея
     oled.fill(0)
+    # Формирование информации для отображения на модуле дисплея
     oled.text("Dyn: " + str(radsense.getRadIntensyDynamic()), 0, 0, 1)
     oled.text("Sta: " + str(radsense.getRadIntensyStatic()), 0, 15, 1)
-    oled.show() 
+    # Показ содержимого на модуле дисплея
+    oled.show()
+    # Пауза 10 секунд
     time.sleep(10)
-    
-    
